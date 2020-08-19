bbbaa8fda0e1fab99fa921614f85fe54376f1f4f
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2020-08-19 17:56:32 +0500

    Npm scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+$("#loginButton").click(function(){
+        $("#loginModal").modal('show');
+    });
+$('#reserveButton').click(function(){
+        $('#reservationModal').modal('show');
+    });
+ });
